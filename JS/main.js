e6ad9b9609eb0eba37ba3051cc478ceb0c6bacda
Kisalaya Kishan <kislay.kishan007@gmail.com>
2022-01-23 02:53:03 +0530

    Minor UI Updates

--- JS/main.js
+++ JS/main.js
@@ -14,12 +14,13 @@ function setQuery(evt){
 }
 
 function getResults(query){
+
     fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
     .then(weather =>{
         return weather.json();
     }).then(displayResults);
 }
-
+getResults('new delhi');
 function displayResults(weather){
     console.log(weather);
     let city = document.querySelector('.location .city .cityName');
@@ -34,6 +35,9 @@ function displayResults(weather){
 
     let weather_el = document.querySelector('.current .weather');
     weather_el.innerText = weather.weather[0].main; 
+
+    // let weather_des = document.querySelector('.description');
+    // weather_des.innerText = weather.weather[0].description; 
 
     let hilow = document.querySelector('.low-temp');
     hilow.innerText = `${Math.round(weather.main.temp_min)}°c `;
@@ -55,15 +59,38 @@ function displayResults(weather){
      
     }
     else if ((weather.weather[0].main)=="Clouds"){
-        document.getElementById("icon").className = "fas fa-cloud windy"; 
+        document.getElementById("app-wrap").classList.remove("sunny");
+        document.getElementById("app-wrap").classList.remove("clear");
+        document.getElementById("app-wrap").classList.remove("rain");
+        document.getElementById("app-wrap").classList.remove("snow");
+        document.getElementById("app-wrap").classList.remove("mist");
+        document.getElementById("icon").className = "fas fa-cloud windy";
+        document.getElementById("app-wrap").classList.add("clouds");
+        // document.querySelector(".app-wrap").classList.add("clouds"); 
       
     }
     else if ((weather.weather[0].main)=="Clear"){
         document.getElementById("icon").className = "fas fa-cloud-sun windy"; 
-      
+        // document.querySelector(".app-wrap").style.background = ""
+        document.getElementById("app-wrap").classList.remove("rain");
+        document.getElementById("app-wrap").classList.remove("sunny");
+        document.getElementById("app-wrap").classList.remove("mist"); 
+        document.getElementById("app-wrap").classList.remove("clouds");
+        document.getElementById("app-wrap").classList.remove("snow");
+        document.getElementById("app-wrap").classList.add("clear");
+
     }
     else if ((weather.weather[0].main)=="Rain"){
+        document.getElementById("app-wrap").classList.remove("sunny");
         document.getElementById("icon").className = "fas fa-cloud-showers-heavy windy"; 
+
+        // document.getElementById("app-wrap").classList.remove("sunny");
+        document.getElementById("app-wrap").classList.remove("clear");
+        document.getElementById("app-wrap").classList.remove("mist");
+        // document.getElementById("icon").className = "fas fa-cloud windy";
+        document.getElementById("app-wrap").classList.remove("clouds");
+        document.getElementById("app-wrap").classList.remove("snow");
+        document.getElementById("app-wrap").classList.add("rain");
       
     }
     else if ((weather.weather[0].main)=="Drizzle"){
@@ -72,6 +99,14 @@ function displayResults(weather){
     }
     else if ((weather.weather[0].main)=="Snow"){
         document.getElementById("icon").className = "fas fa-snowflake windy"; 
+        
+        document.getElementById("app-wrap").classList.remove("sunny");
+        document.getElementById("app-wrap").classList.remove("clear");
+        document.getElementById("app-wrap").classList.remove("mist");
+        // document.getElementById("icon").className = "fas fa-cloud windy";
+        document.getElementById("app-wrap").classList.remove("clouds");
+        document.getElementById("app-wrap").classList.remove("rain");
+        document.getElementById("app-wrap").classList.add("snow");
       
     }
     else if ((weather.weather[0].main)=="Thunderstorm"){
@@ -83,7 +118,13 @@ function displayResults(weather){
       
     }
     else if ((weather.weather[0].main)=="Mist"){
-        document.getElementById("icon").className = "fas fa-water windy"; 
+        document.getElementById("icon").className = "fas fa-water windy";
+        document.getElementById("app-wrap").classList.remove("clouds"); 
+        document.getElementById("app-wrap").classList.remove("sunny"); 
+        document.getElementById("app-wrap").classList.remove("clear"); 
+        document.getElementById("app-wrap").classList.remove("rain");
+        document.getElementById("app-wrap").classList.remove("snow");
+        document.getElementById("app-wrap").classList.add("mist"); 
       
     }
     else if ((weather.weather[0].main)=="Fog"){
@@ -91,7 +132,8 @@ function displayResults(weather){
       
     }
     else {
-        document.getElementById("icon").className = "fas fa-sun windy"; 
+        document.getElementById("icon").className = "fas fa-sun windy";
+        document.querySelector(".app-wrap").append.className = "sunny"; 
       
     }
 
